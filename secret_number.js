58e0cda3090e2351707c85b95338daea69bda7bb
Raymond Luong <raymond.n.luong@gmail.com>
2016-10-12 16:25:33 -1000

    Implement random number generator using closure scope

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+	let randomNumber = getRandomArbitrary(0, 1000001)
+
+	function getRandomArbitrary(min, max) {
+  		return Math.random() * (max - min) + min;
+	}
+
+  	return  function secretNumber() {
+  				return randomNumber
+  			}
 };
